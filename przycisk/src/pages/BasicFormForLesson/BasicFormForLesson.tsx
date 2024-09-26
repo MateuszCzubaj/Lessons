f05272e8be28491ed05602f1ab63cb215b4867d8
SDISOLUTION\mateusz.czubaj <mateusz.czubaj@sdisolution.pl>
2024-09-26 13:41:57 +0200

    updated form

--- przycisk/src/pages/BasicFormForLesson/BasicFormForLesson.tsx
+++ przycisk/src/pages/BasicFormForLesson/BasicFormForLesson.tsx
@@ -1,5 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import Form from "../../components/Form";
+
 const BasicFormForLessons = () => {
-  return <div className=""></div>;
+  return (
+    <Box className="" height={"100vh"}>
+      <Form />
+    </Box>
+  );
 };
 
 export default BasicFormForLessons;
